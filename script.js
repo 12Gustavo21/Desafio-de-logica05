0484d474a9b5f083738d804458cc1a7c4645b366
12Gustavo21 <gustavo927318@gmail.com>
2022-06-22 12:10:31 -0300

    Desafio-de-logica05

---
+++ script.js
@@ -0,0 +1,24 @@
+//Questão 01
+let primeiroArray = ["Eu", "amo", "gatinhos", "sim", "amo"];
+
+console.log(primeiroArray);
+
+//Questão 02
+const novoElemento = primeiroArray.unshift("Sim");
+console.log(primeiroArray);
+
+//Questão 03
+const removerElemento = primeiroArray.pop();
+console.log(primeiroArray);
+
+//Questão 04
+const novoElemento1 = primeiroArray.push("eu", "amo");
+console.log(primeiroArray);
+
+//Questão 05
+const removerElemento1 = primeiroArray.shift();
+console.log(primeiroArray);
+
+//Questão 06
+let numbers = [7, 5, 6, 3, 8, 9, 2, 1, 4];
+console.log(numbers.sort());
